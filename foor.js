5374c772b86761d452223dc4621b74aac98e3fce
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-09-26 09:01:24 +0300

    initial commit

---
+++ foor.js
@@ -0,0 +1,61 @@
+//10 masala
+// let n = 5;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum += 2 * i - 1;
+//   console.log(sum);
+// }
+
+//11 masala
+
+// let n = 5;
+// let a = 2;
+// let sum = 0;
+
+// for (let i = a; i <= n; i++) {
+//   sum = Math.pow(a, i);
+// }
+
+// console.log(sum);
+
+//12 masala
+
+// let n = 5;
+// let a = 2;
+// let sum = 0;
+
+// for (let i = a; i <= n; i++) {
+//   sum = Math.pow(a, i);
+//   console.log(sum);
+// }
+
+//13 masala
+
+// let n = 4;
+// let a = 2;
+// let sum = 0;
+// let counter = 0;
+
+// for (let i = a; i <= n; i++) {
+//   sum = Math.pow(a, i);
+//   counter += sum;
+//   console.log(sum);
+// }
+
+// console.log(counter);
+
+//14 masala
+
+// let n = 4;
+// let a = 2;
+// let sum = 0;
+// let counter = 0;
+
+// for (let i = a; i <= n; i++) {
+//   sum = Math.pow(a, i);
+//   counter += sum;
+//   console.log(sum);
+// }
+
+// console.log(counter);
