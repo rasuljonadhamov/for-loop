edafb858653b4124ec7a48ad519338016b2229e0
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-09-26 10:15:35 +0300

    solve for exercises

--- foor.js
+++ foor.js
@@ -48,14 +48,89 @@
 //14 masala
 
 // let n = 4;
-// let a = 2;
 // let sum = 0;
-// let counter = 0;
+// let fact = 1;
+// for (i = 1; i <= n; i++) {
+//   fact *= i;
+//   sum += fact;
+// }
+// console.log(sum);
 
-// for (let i = a; i <= n; i++) {
-//   sum = Math.pow(a, i);
-//   counter += sum;
-//   console.log(sum);
+//15 masala
+
+// let n = 4;
+// let k = 6;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum += i ** k;
 // }
 
-// console.log(counter);
+// console.log(sum);
+
+//16 masala
+
+// let n = 4;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum += i ** i;
+// }
+
+// console.log(sum);
+
+//17 masala
+
+// let a = 2;
+// let b = 4;
+
+// for (let i = a; i <= b; i++) {
+//   for (let k = 1; k < i; k++) {
+//     console.log(i);
+//   }
+// }
+
+//18 masala
+
+// let n = 24;
+// let sum = 0;
+// let counter = 0;
+// for (i = 1; i <= n; i++) {
+//   if (n % i === 0) {
+//     console.log(i);
+//     sum += i;
+//     counter++;
+//   }
+// }
+// console.log("Bo'luvchilari soni", counter, "ta");
+// console.log("Bo'luvchilari yigindisi", sum);
+
+//19 masala
+
+// let n = 17;
+// let counter = 0;
+// for (i = 1; i <= n; i++) {
+//   if (n % i === 0) {
+//     counter++;
+//   }
+// }
+
+// if (counter === 2) {
+//   console.log("Bu son tub son");
+// } else {
+//   console.log("Tub son emas");
+// }
+
+// 20 masala
+
+// let n = 5;
+// let text = "";
+
+// for (let i = 1; i <= n; i++) {
+//   for (let k = 1; k <= i; k++) {
+// console.log(k);
+//     text += " " + k + " ";
+//   }
+//   console.log(text);
+//   text = "";
+// }
